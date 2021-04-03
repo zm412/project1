cb4e930dfaa3526a3536e26782d2e993657292de
zm412 <zm412412@gmail.com>
2021-04-03 10:38:06 -0400

    add page is working

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django import forms
+
+from . import util
+
+class newItemCreate(forms.Form):
+    title = forms.CharField()
+    article = forms.CharField()
+
+
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def add(request):
+    if request.method == 'POST':
+        newArticle = newItemCreate(request.POST)
+        print(newArticle, 'print')
+        title = newArticle.cleaned_data['title']
+        content = newArticle.cleaned_data['article']
+        util.save_entry(title, content)
+        return render(request, 'encyclopedia/index.html', {
+            "entries": util.list_entries()
+        })
+    else:
+        return render(request, 'encyclopedia/add.html', {
+            "form": newItemCreate()
+        })
+
+def random(request):
+    return render(request, "encyclopedia/random.html", {
+        "entries": util.list_entries()
+    })
+
