c0dd66c1cd0bbbec27e2e15149ad6c657e8db0fe
zm412 <zm412412@gmail.com>
2021-04-04 12:15:41 -0400

    started page search

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("add/", views.add, name="add"),
     path("random", views.random, name="random"),
+    path("search/", views.search, name="search"),
     path("<str:title>/", views.article)
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render
 from django import forms
 from django.utils.safestring import mark_safe
+from django.http import HttpResponse
 import markdown
 
 from . import util
 
-class newItemCreate(forms.Form):
+class NewItemCreate(forms.Form):
     title = forms.CharField()
     article = forms.CharField()
 
+
+class SearchArticle(forms.Form):
+    q = forms.CharField(label='')
 
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "search_form": SearchArticle()
     })
 
 def add(request):
     if request.method == 'POST':
-        newArticle = newItemCreate(request.POST)
-        print(newArticle, 'print')
+        newArticle = NewItemCreate(request.POST)
         title = newArticle.cleaned_data['title']
         content = newArticle.cleaned_data['article']
         util.save_entry(title, content)
@@ -29,7 +33,7 @@ def add(request):
         })
     else:
         return render(request, 'encyclopedia/add.html', {
-            "form": newItemCreate()
+            "form": NewItemCreate()
         })
 
 def random(request):
@@ -40,20 +44,38 @@ def random(request):
 
 def article(request, title):
     text = util.get_entry(title)
-    md = markdown.Markdown()
-    article = mark_safe(md.convert(text))
-    print(text)
-    print(article)
-
-    if article:
+    if text:
+        md = markdown.Markdown()
+        article = mark_safe(md.convert(text))
         return render(request, 'encyclopedia/article.html', {
             "article": article
         })
     else:
-        return HttpResponse("Error")
+        return render(request, 'encyclopedia/article.html', {
+            "article": 'Wiki does not have an arcticle with this name'
+        })
 
 
-
+def search(request):
+    if request.method == 'POST':
+        form_data = SearchArticle(request.POST)
+        if form_data.is_valid():
+            title = form_data.cleaned_data['q']
+            text = util.get_entry(title)
+            if text:
+                md = markdown.Markdown()
+                article = mark_safe(md.convert(text))
+                return render(request, 'encyclopedia/search.html', {
+                    "search_result": article,
+                })
+            else:
+                return render(request, 'encyclopedia/search.html', {
+                    "article": 'Wiki does not have an arcticle with this name'
+                })
+        else:
+            return render(request, 'encyclopedia/index.html')
+    else:
+        return render(request, 'encyclopedia/index.html')
 
 
 
